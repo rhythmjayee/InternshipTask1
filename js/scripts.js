dececb94cc297b3f3b979f07b20988229f573dcb
anujarora0502 <43067216+anujarora0502@users.noreply.github.com>
2020-10-07 02:02:06 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ $(function () {
 
   (function() {
     'use strict';
-    window.addEventListener('load', function() {
+    window.addEventListener('load', ()=>{
       // Fetch all the forms we want to apply custom Bootstrap validation styles to
       var forms = document.getElementsByClassName('needs-validation');
       // Loop over them and prevent submission
       var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
+        form.addEventListener('submit', (event)=>{
           if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
@@ -22,7 +22,7 @@ $(function () {
   })();
 
 
-  $('#paid').on('click',function(){
+  $('#paid').on('click',()=>{
     $('#next1').removeAttr("disabled");
     $('#next1').attr({
       'data-toggle':"modal" ,
@@ -33,13 +33,13 @@ $(function () {
     $('#next1').removeClass("d-none")
   });
 
-  $('#free').on('click',function(){
+  $('#free').on('click',()=>{
     $('#next1').addClass("d-none");
     $('#modal1-done').removeClass('d-none');
     
   });
 
-  $('#single').on('click',function(){
+  $('#single').on('click',()=>{
     $('#next2').removeAttr("disabled");
     $('#next2').attr({
       'data-toggle':"modal" ,
@@ -48,7 +48,7 @@ $(function () {
     });
   });
 
-  $('#multiple').on('click',function(){
+  $('#multiple').on('click',()=>{
     $('#next2').removeAttr("disabled");
     $('#next2').attr({
       'data-toggle':"modal" ,
@@ -57,10 +57,10 @@ $(function () {
     });
   });
 
-  $('#online').on('click',function(){
+  $('#online').on('click',()=>{
     $('offline').removeAttr("required");
   });
-  $('#offnline').on('click',function(){
+  $('#offnline').on('click',()=>{
     $('#online').removeAttr("required");
   });
 
@@ -73,7 +73,7 @@ $(function () {
   // $('#totalCost').text(Total);
 
 //category button
-  $('#category').on('click',function(){
+  $('#category').on('click',()=>{
     $('#b1').removeClass("d-none");
     // $('#bb1').removeClass('d-none');
     var out='<div class="row" id="r'+(++count)+'"> <div class="col-1"> <span>'+(count)+'.</span> </div><div class="col-10"> <div class="card" style="width:20em;"> <div class="card-header" id="h'+(count)+'"> <div class="row"> <div class="col-10"> <div class="d-flex justify-content-start mr-auto"> <h6><span id="s'+(count)+'"></span></h6> </div></div><div class="col-1  " id="b'+(count)+'"> <button type="button" class="btn btn-light ml-auto d-flex" data-toggle="collapse" data-target="#c'+(count)+'" aria-expanded="true" aria-controls="collapseOne"> <i class="fa fa-arrow-up"></i> </button> </div></div></div><div id="c'+(count)+'"class="collapse show" aria-labelledby="h'+(count)+'" data-parent="#accordion"> <div class="card-body"> <div class="form-group"> <div class="row"> <div class="col-10"> <label for="Category'+(count)+'">Category?</label> <input type="text" class="form-control" id="Category'+(count)+'" style="width: 15em;" placeholder="Example:Entry,food etc"> </div><div class="col-1"id="bb'+(count)+'"> <button role="button" class=" btn btn-light"><i class="fa fa-minus"></i></button> </div></div></div><div class="form-group"> <label for="Description'+(count)+'">Description</label> <input type="text" class="form-control" id="Description'+(count)+'" style="width: 15em;"placeholder="Description"> </div><div class="form-group"> <label for="p'+(count)+'">Price of ticket?</label> <input type="number" class="form-control" style="width: 15em;" id="p'+(count)+'" placeholder="Price per ticket" required> </div></div></div></div></div></div>'
@@ -81,7 +81,7 @@ $(function () {
 
 
  //for total cost
- $('#p'+count).on('change',function(){
+ $('#p'+count).on('change',()=>{
         
   var price=$('#p'+count).val();
   total=Number(total)+Number(price);
@@ -89,7 +89,7 @@ $(function () {
 });
 
     //minus button
-    $('#bb'+count).on('click',function(){
+    $('#bb'+count).on('click',()=>{
       var price=$('#p'+count).val();
       total=Number(total)-Number(price);
       $('#totalCost').text(total);
@@ -98,7 +98,7 @@ $(function () {
       });
 
 //for collapse title
-      $("#c"+count).on('hidden.bs.collapse',function(){
+      $("#c"+count).on('hidden.bs.collapse',()=>{
         var title=$('#Category'+count).val();
         var price=$('#p'+count).val();
         // total=Number(total)+Number(price);
@@ -111,7 +111,7 @@ $(function () {
       });
 
       //for hide title on show collapse
-      $("#c"+count).on('shown.bs.collapse',function(){
+      $("#c"+count).on('shown.bs.collapse',()=>{
           $('#s'+count).addClass('d-none');
           var UpDown='#b'+(count)+' button i';
         $(UpDown).toggleClass('fa-arrow-down fa-arrow-up');
